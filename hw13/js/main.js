c0ad72690e674fd7b94e01bc8321be49a8f625f4
chethor2112 <153759836+chethor2112@users.noreply.github.com>
2025-10-26 23:05:31 +0400

    13.2 дом / 2 мод, 136 урок

--- hw13/js/main.js
+++ hw13/js/main.js
@@ -7,18 +7,34 @@
     const burgerIcon = e.target.closest('.burger-icon')
     const bugerNavLink = e.target.closest('.nav__link')
 
-    if (!burgerIcon && !bugerNavLink ) return 
+    if (!burgerIcon && !bugerNavLink) return
 
-    if (document.documentElement.clientWidth > 900) return 
-    
+    if (document.documentElement.clientWidth > 900) return
+
     if (!document.body.classList.contains('body--opened-menu')) {
       document.body.classList.add('body--opened-menu')
-    } else{
+    } else {
       document.body.classList.remove('body--opened-menu')
     }
+  }
 
+  const modal = document.querySelector('.modal')
+  const modalButton = document.querySelector('.about__img-button')
 
+  modalButton.addEventListener('click', openModal)
+  modal.addEventListener('click', closeModal)
 
+  function openModal(e) {
+    e.preventDefault()
+    document.body.classList.toggle('body--opened-modal')
+  }
+    function closeModal(e) {
+    e.preventDefault()
 
+    const target = e.target
+
+    if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+      document.body.classList.remove('body--opened-modal')
+    }
   }
 })()
