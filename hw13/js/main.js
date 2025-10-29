a076350bc27334c986f1d94c7a0b84203c4d7d10
chethor2112 <153759836+chethor2112@users.noreply.github.com>
2025-10-29 14:56:05 +0400

    13.6 дом / 2 мод, 155 урок

--- hw13/js/main.js
+++ hw13/js/main.js
@@ -153,5 +153,10 @@
     }
   });
 
+  // маска для телефона
+
+  const telInputs = document.querySelectorAll('input[type="tel"]');
+  const im = new Inputmask ('+7 (999) 999-99-99')
+  im.mask(telInputs)
 
 })()
